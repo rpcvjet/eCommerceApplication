8dba53280432023c17118dbeebc94f0d0414387e
rpcvjet <kenneth.ashley@gmail.com>
2021-02-08 14:54:00 -0500

    cart and user controllers complete

--- starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -5,13 +5,17 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -55,5 +59,50 @@ public class UserControllerTest {
         assertEquals("thisIsHashed", user.getPassword());
     }
 
-    
+    @Test
+    public void findUserByIdTest() throws Exception {
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("findusertest");
+        userRequest.setPassword("testPassword");
+        userRequest.setConfirmpassword("testPassword");
+        System.out.println(userRequest.getUsername());
+        ResponseEntity<User> response = userController.createUser(userRequest);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User user = new User();
+        user.setUsername("findusertest");
+        Optional<User> optionalUser = Optional.of(user);
+        when(userRepository.findById(anyLong())).thenReturn((optionalUser));
+        response = userController.findById(0L);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void testFindByUsername()
+    {
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("Chris");
+        userRequest.setPassword("testPassword");
+        userRequest.setConfirmpassword("testPassword");
+        ResponseEntity<User> response = userController.createUser(userRequest);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        User user = new User();
+        user.setUsername("test");
+        when(userRepository.findByUsername(anyString())).thenReturn(user);
+        response = userController.findByUserName(response.getBody().getUsername());
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        user = response.getBody();
+        assertNotNull(user);
+        assertEquals("test", user.getUsername());
+
+    }
 }
